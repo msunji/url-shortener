7fbc4a115ecde108cbdbae584710a97bbadca42e
Marge <marge.consunji@gmail.com>
2022-01-05 15:02:08 +0800

    Import Footer and Cta components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import './styles/main.scss';
+import './styles/_reset.scss';
+import Cta from './components/Cta';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div>
       test
+      <Cta />
+      <Footer />
     </div>
   );
 }
