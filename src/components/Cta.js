53f9150583d9028f6a1a72d5ff9c7f95d7589b73
Marge <marge.consunji@gmail.com>
2022-01-05 15:02:57 +0800

    Import Button component, add bg styling, bottom margin to text

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,10 +1,12 @@
 import '../styles/components/Cta.scss';
+import Button from './Button';
 
 const Cta = () => {
   return (
     <section>
-      <div className="cta-container">
+      <div className="container">
         <h2>Boost your links today</h2>
+        <Button color="cyan" shape="pill">Get Started</Button>
       </div>
     </section>
   )
