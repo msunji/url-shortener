ad64f174242466392988fd57ef8580c7dcee49ca
Marge <marge.consunji@gmail.com>
2022-01-06 16:01:03 +0800

    Add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,8 @@
-import { ReactComponent as Logo } from '../assets/img/logo.svg'; 
+import { ReactComponent as Logo } from '../assets/img/logo.svg';
+import { ReactComponent as FBIcon } from '../assets/img/icon-facebook.svg';
+import { ReactComponent as IGIcon } from '../assets/img/icon-instagram.svg';
+import { ReactComponent as TWIcon } from '../assets/img/icon-twitter.svg';
+import { ReactComponent as PinIcon } from '../assets/img/icon-pinterest.svg';
 import '../styles/components/Footer.scss';
 
 const Footer = () => {
@@ -25,6 +29,12 @@ const Footer = () => {
           <span>Link Shortening</span>
           <span>Branded Links</span>
           <span>Analytics</span>
+        </div>
+        <div className="icon-container">
+          <FBIcon />
+          <TWIcon />
+          <PinIcon/>
+          <IGIcon />
         </div>
       </div>
     </footer>
