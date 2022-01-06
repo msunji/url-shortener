9e9fe22ac1494b69e15c78e54ca6af4f3d7032b6
Marge <marge.consunji@gmail.com>
2022-01-06 15:32:41 +0800

    Import Hero component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './styles/main.scss';
 import './styles/_reset.scss';
+import Hero from './components/Hero';
+import Statistics from './components/Statistics';
 import Cta from './components/Cta';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      test
+      <Hero />
+      <Statistics />
       <Cta />
       <Footer />
     </div>
