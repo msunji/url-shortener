f0717a3fc5d2fbcff3c2f0463f782ee885beeb80
Marge <marge.consunji@gmail.com>
2022-01-07 10:40:47 +0800

    Make URL shortener component

---
+++ src/components/Shortener.js
@@ -0,0 +1,16 @@
+import '../styles/components/Shortener.scss';
+
+const Shortener = () => {
+  return (
+      <div className="shortener">
+        <div className="shortener__form">
+          Test
+        </div>
+        <div className="shortener__links">
+
+        </div>
+      </div>
+  )
+}
+
+export default Shortener;
