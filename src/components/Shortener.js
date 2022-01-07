5f46755f152679027fe317bfce953deeded359d6
Marge <marge.consunji@gmail.com>
2022-01-07 13:26:49 +0800

    Make list item its own component

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,14 +1,29 @@
 import '../styles/components/Shortener.scss';
+import Button from './Button';
+
+const Link = () => {
+  return (
+    <li className="link">
+      <p className="link--original">
+          https://www.w3schools.com/tags/ref_httpmethods.asp</p> 
+      <p className="link--shortened">shrtco.de/KCveN</p>
+      <Button color="cyan" shape="rounded">Copy</Button>
+  </li>
+  )
+}
 
 const Shortener = () => {
   return (
       <div className="shortener">
-        <div className="shortener__form">
-          Test
+        <div className="shortener__container">
+          <form>
+            <input type="text" placeholder="Shorten a link here..."></input>
+            <Button color="cyan" shape="rounded">Shorten It!</Button>
+          </form>
         </div>
-        <div className="shortener__links">
-
-        </div>
+        <ul className="shortener__links">
+          <Link />
+        </ul>
       </div>
   )
 }
