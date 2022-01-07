02f5ff3af4df107d97a2ed7fa434d42f7b404075
Marge <marge.consunji@gmail.com>
2022-01-07 15:54:58 +0800

    Use shrtcode API to shorten URLs

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import '../styles/components/Shortener.scss';
 import Button from './Button';
 
@@ -13,11 +14,27 @@ const Link = () => {
 }
 
 const Shortener = () => {
+  const [ link, setLink ] = useState('');
+  const [ shortenedLinks, setShortenedLinks ] = useState([]);
+
+  const shortenUrl = (e) => {
+    e.preventDefault();
+    fetch(`https://api.shrtco.de/v2/shorten?url=${link}`, {
+            method: 'POST',
+        }).then(res => res.json())
+        .then((({
+            result
+        }) => setShortenedLinks({
+            original: result.original_link,
+            short: result.full_short_link
+        })));
+  };
+
   return (
       <div className="shortener">
         <div className="shortener__container">
-          <form>
-            <input type="text" placeholder="Shorten a link here..."></input>
+          <form onSubmit={shortenUrl}>
+            <input type="text" value={link} placeholder="Shorten a link here..." onChange={(e) => setLink(e.target.value)} required></input>
             <Button color="cyan" shape="rounded">Shorten It!</Button>
           </form>
         </div>
