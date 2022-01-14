44854b017c7d90d0a60418b44147abcd5778e6c8
Marge <marge.consunji@gmail.com>
2022-01-14 15:21:01 +0800

    Add copied class if Copy button is clicked

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import '../styles/components/Button.scss';
 
-const Button = ({ color, shape, children }) =>{
+const Button = ({ color, shape, children, copied }) =>{
   return (
-    <button className={`button-${color} ${shape}`} >{children}</button>
+    <button className={`button-${color} ${shape} ${copied}`} >{children}</button>
   )
 }
 export default Button;
