98755c787c1ff6d234adaa7706c818caf24e5ef2
Marge <marge.consunji@gmail.com>
2022-01-14 15:00:46 +0800

    Revert to original button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import '../styles/components/Button.scss';
 
 const Button = ({ color, shape, children }) =>{
   return (
-    <button className={`button-${color} ${shape}`}>{children}</button>
+    <button className={`button-${color} ${shape}`} >{children}</button>
   )
 }
 export default Button;
